0397957ae2598c83eabe80efb9114f6ff4cd49ad
Simon GUILLEMOT <simon.guillemot@orange.com>
2021-07-16 16:06:28 +0200

    Hexagon : deploy production script

--- deployProduction.sh
+++ deployProduction.sh
@@ -3,9 +3,10 @@
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $BASEDIR
+chown -R kimai:www-data .
+
 ./bin/console kimai:update
 
-chown -R :www-data .
 chmod -R g+r .
 chmod -R g+rw var/
 chmod -R g+rw public/avatars/
