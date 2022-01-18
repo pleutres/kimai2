4d839346dd42157f266a5fe7d8c9b138dab623e1
Simon GUILLEMOT <simon.guillemot@hexagon-perf.com>
2022-01-18 22:06:53 +0100

    Hexagon : update deploy commands

--- deployProduction.sh
+++ deployProduction.sh
@@ -5,6 +5,7 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $BASEDIR
 chown -R kimai:www-data .
 
+composer install --no-dev --optimize-autoloader -n
 ./bin/console kimai:update
 
 chmod -R g+r .
